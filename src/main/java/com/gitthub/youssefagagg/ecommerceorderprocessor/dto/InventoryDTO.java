ffa16219c71ef1efa8639018ecec57d26a37e9f2
Youssef Agagg <youssefagagg@gmail.com>
2025-06-09 20:19:19 +0300

    remove inheritance from AbstractAuditingEntityDto in InventoryDTO

--- src/main/java/com/gitthub/youssefagagg/ecommerceorderprocessor/dto/InventoryDTO.java
+++ src/main/java/com/gitthub/youssefagagg/ecommerceorderprocessor/dto/InventoryDTO.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
@@ -16,8 +15,7 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-@EqualsAndHashCode(callSuper = true)
-public class InventoryDTO extends AbstractAuditingEntityDto {
+public class InventoryDTO {
 
   private Long id;
 
